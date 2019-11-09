387734934f431b93780b0dd56fe02a2f70b51e8e
Gabriel Heinrich <gabriel@codecampleipzig.de>
2019-11-09 17:23:19 +0100

    Add developer team all by myself

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const developerTeam = ['Gabe'];
+const developerTeam = ['Ani', 'Bea', 'Bjorn', 'Gabe', 'Mariana', 'Nick', 'Nico', 'Simona'];
 
-console.log("Git Training Field");
+
+console.log("Git Training Field!");
 console.log (developerTeam);
